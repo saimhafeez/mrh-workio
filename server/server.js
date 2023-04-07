9f05d75e11519aede3fbebcc1af2e04e21302f31
saimhafeez2020 <saimhafeez2020@gmail.com>
2023-04-07 08:28:27 +0500

    changes to server config

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 // for production
 const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.use(express.json())
 
@@ -59,7 +59,7 @@ app.use('/api/v1/jobs', authenticateUser, jobsRoutes)
 
 // for production
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
 
